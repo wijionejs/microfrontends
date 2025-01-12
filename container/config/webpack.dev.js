a88793ba870ced10c6f0dd97af35ee5865d78651
Volodymyr Baranchuk <crackhead199701@gmail.com>
2025-01-12 15:00:39 +0200

    add basic strucure and webpack configs for container

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
